cde76bea4e56427353fafb6085ed02025d560a8f
Ritchie Blair <ritchie@infinityrobot.com>
2016-06-20 10:34:39 +1000

    :wrench: Add post_count attribute to Spooky::Tag

--- lib/spooky/tags.rb
+++ lib/spooky/tags.rb
@@ -1,6 +1,6 @@
 module Spooky
   class Tag < Spooky::Base
-    attr_reader :name, :description, :image, :hidden, :parent
+    attr_reader :name, :description, :image, :hidden, :parent, :post_count
 
     def initialize(attrs = {})
       @name = attrs["name"]
@@ -8,6 +8,7 @@ module Spooky
       @image = attrs["image"]
       @hidden = attrs["hidden"]
       @parent = attrs["parent"]
+      @post_count = attrs.dig("count", "posts")
 
       super(attrs)
     end
